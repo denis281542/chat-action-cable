10f4e77a87d7e87aa273cffc3682d1984bb7b6a9
denis281542 <deniskuimov@mail.ru>
2019-10-17 23:20:40 +0500

    Edit environment.js

--- app/javascript/packs/example.js
+++
@@ -1,3 +0,0 @@
-//= reguire jquery3
-//= reguire bootstrap
-//= reguire papper
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+environment.plugins.prepend('Provide', 
+  new webpack.ProvidePlugin({
+    $: 'jquery/scr/jquery',
+    jQuery: 'jquery/scr/jquery'
+  })
+)
+
 module.exports = environment
